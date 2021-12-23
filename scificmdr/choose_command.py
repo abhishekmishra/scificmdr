7f542f62887d62af980cbff1d79f692ffbc3f02e
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-12-23 13:27:30 +0530

    added rapidfuzz based command match

--- scificmdr/choose_command.py
+++ scificmdr/choose_command.py
@@ -1,33 +1,54 @@
 import PySimpleGUI as sg
-
+from rapidfuzz.process import *
 
 class CommandRegister:
     def __init__(self):
         self.commands = {}
+        self.desc_text = {}
 
     def register(self, name, description=None):
         """
-        Registers a command
+        Registers a command.
+        The command is converted to lowercase before adding.
+        All lookup is lowercase going forward.
         """
-        if self.is_command(name):
-            raise KeyError(name + " is already a registered command.")
+        name_lower = name.lower()
+        if self.is_command(name_lower):
+            raise KeyError(name_lower + " is already a registered command.")
         else:
-            self.commands[name] = description
+            self.commands[name_lower] = description
+            self.desc_text[name_lower] = name_lower + ' ' + description.lower()
 
     def deregister(self, name):
         """
         Deregisters (removes) command
         """
-        self.commands.pop(name)
+        self.commands.pop(name.lower())
+        self.desc_text.pop(name.lower())
 
     def is_command(self, name):
-        return name in self.commands.keys()
+        return name.lower() in self.commands.keys()
 
+    def match(self, input):
+        results = extract(input, self.desc_text, score_cutoff=75)
+        return results
 
 COMMANDS = CommandRegister()
 
 
-def choose_command(title="Choose command", allow_unlisted=True):
+def register_command(name, description=None):
+    COMMANDS.register(name, description)
+
+
+def deregister_command(name):
+    COMMANDS.deregister(name)
+
+
+def is_command(name):
+    return COMMANDS.is_command(name)
+
+
+def choose_command(title="Choose command", allow_unlisted=True, commands=COMMANDS):
     cmd = None
 
     layout = [[sg.Input(enable_events=True, focus=True, pad=(0, 0), k="command_query")]]
@@ -65,5 +86,12 @@ def choose_command(title="Choose command", allow_unlisted=True):
 
 
 if __name__ == "__main__":
+    register_command("apple", "is a red fruit")
+    register_command("cherry", "a red fruit")
+    register_command("banana", "is a yellow fruit")
+    register_command("pineapple", "is a yellow and green fruit")
+    register_command("brinjal", "is a purple vegetable")
+    print(COMMANDS.match('red'))
+    exit(0)
     x = choose_command()
     print(x)
